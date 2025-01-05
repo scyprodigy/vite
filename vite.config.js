68ab5d1a9b0b4b946214a8970f74bc50c90a03cb
Red-Yu <chris830321@gmail.com>
2025-01-05 22:02:10 +0800

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Phantasia",
+  base: "/<Phantasia>",
   plugins: [vue()],
   resolve: {
     alias: {
