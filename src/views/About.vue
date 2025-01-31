73561072896b2ac9b58df0fb1fd923f6080f9624
Red-Yu <chris830321@gmail.com>
2025-01-31 21:38:38 +0800

    add_about頁面初步切版

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,14 +4,86 @@
 
 <template>
   <div class="wrapper">
-    <div class="aboutUs">
-      <img src="" alt="" class="aboutUsImg" />
-    </div>
-    <div class="ourOrigin">
-      <img src="" alt="" class="ourOriginImg" />
-    </div>
-    <div class="ourServices">
-      <img src="" alt="" class="ourServicesImg" />
+    <div class="about">
+      <div class="content">
+        <div class="leftContent">
+          <div class="contentBox">
+            <div class="slogon">
+              <h1>Reading</h1>
+              <h2>is more than words—</h2>
+              <h6>An interactive journey of exploration.</h6>
+            </div>
+            <div class="menu">
+              <div class="aboutUs_button">
+                <button class="aboutUs_button">About Us</button>
+              </div>
+              <div class="ourOrigin_button">
+                <button class="ourOrigin_button">Our Origin</button>
+              </div>
+              <div class="ourServices_button">
+                <button class="ourServices_button">Our Services</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="rightContent">
+          <div class="aboutContents">
+            <div class="aboutUsContent">
+              <h3>About Us</h3>
+              <span>
+                <p>
+                  In today’s fast-paced digital era, we are committed to
+                  creating a safe and nurturing online space for parents and
+                  children. Our platform focuses on the creation and sharing of
+                  interactive picture books, blending stories, special effects,
+                  and engaging interactions to transform reading into a new way
+                  for children to explore the world.
+                </p>
+                <p>
+                  We offer more than just one-page story websites; we provide a
+                  platform full of creativity and possibilities:
+                </p>
+                <ul>
+                  <li>
+                    Designed for creators: Anyone can upload their images and
+                    story texts, and we’ll help transform them into vibrant,
+                    interactive picture books while offering sales and
+                    profit-sharing services.
+                  </li>
+                  <li>
+                    Supporting children’s growth: In an age flooded with
+                    information, we believe reading should be both safe and
+                    enjoyable. Our interactive picture books aim to inspire
+                    children’s creativity, nurture their imagination, and give
+                    parents peace of mind.
+                  </li>
+                </ul>
+
+                <p>
+                  Our vision is to merge technology with storytelling, becoming
+                  a trusted companion on children’s journey of growth and
+                  bringing warmth and hope to the digital age. Join us and
+                  discover the endless possibilities of interactive picture
+                  books!
+                </p>
+              </span>
+            </div>
+            <div class="bgBlack"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="aboutUsImg">
+        <img src="../Assets/Day/aboutUsImg.jpg" alt="" class="aboutUsImg" />
+      </div>
+
+      <div class="ourOriginImg">
+        <img src="" alt="" class="ourOriginImg" />
+      </div>
+
+      <div class="ourServicesImg">
+        <img src="" alt="" class="ourServicesImg" />
+      </div>
     </div>
   </div>
 </template>
